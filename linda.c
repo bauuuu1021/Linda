c2caa8a6e247c47375eeebdeb8fa4888a5317cf1
bauuuu1021 <bboy010554@icloud.com>
2019-11-11 07:42:02 +0800

    Prototype of server logic

--- linda.c
+++ linda.c
@@ -1,15 +1,80 @@
-#include <stdio.h>
-#include <string.h>
-#include <omp.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include <omp.h>
+#include <map>
+extern "C" {
+#include <string.h>
+#include <stdio.h>
+}
+
+using namespace std;
+#define MAX_LEN 1024
+
+enum TYPE {
+	STR,
+	INT,
+	VAR
+};
+
+typedef struct element {
+	int type;
+        int client_id;
+	union {
+		char content_str[MAX_LEN];
+		int content_int;
+		char var_name[MAX_LEN];
+	};
+} element_t;
 
 omp_lock_t simple_lock;
+std::vector<int> client_vec;
+bool nextRnd = true;
+
+void master () {
+    while (1) {
+        int id;
+        //while (!omp_test_lock(&simple_lock));
+        printf("master request\n");
+        scanf("%d", &id);
+        client_vec.at(id) = 1;
+        //omp_unset_lock(&simple_lock);
+    }
+}
+
+void client (int thread_id) {
+    while (nextRnd) {
+        while (!client_vec[thread_id]) ;
+        printf("Thread %d - acquired simple_lock\n", thread_id);
+        client_vec[thread_id] = 0; 
+    }
+}
 
 int main () {
 	int numThread;
 	std::cin >> numThread;
+    client_vec.resize(numThread + 1, 0);
+
+    omp_init_lock(&simple_lock);
+
+    #pragma omp parallel num_threads(numThread+1)
+    {
+        int tid = omp_get_thread_num();
+
+        // master
+        if (tid == 0) {
+            master();
+        } 
+        // client
+        else {
+            client(tid);
+        }
+    }
+    
+    omp_destroy_lock(&simple_lock);
+}
+
 #if 0
  	omp_init_lock(&simple_lock);
 
@@ -40,23 +105,3 @@ sleep(1);
 
     omp_destroy_lock(&simple_lock);
 #endif
-    #pragma omp parallel num_threads(numThread+1)
-        {
-            int tid = omp_get_thread_num();
-
-            // master
-            if (tid == 0) {
-                while (1) {
-                    printf("master\n");
-                    sleep(2);
-                }
-            } 
-            // client
-            else {
-            while (1) {
-                printf("Thread %d - acquired simple_lock\n", tid);
-                sleep(1);
-            }
-            }
-        }
-}
--- parser.c
+++ parser.c
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <map>
 extern "C" {
 #include <string.h>
 #include <stdio.h>
@@ -17,6 +18,7 @@ enum TYPE {
 
 typedef struct element {
 	int type;
+        int client_id;
 	union {
 		char content_str[MAX_LEN];
 		int content_int;
@@ -26,7 +28,8 @@ typedef struct element {
 
 int main () {
         char *p;
-	list<list<element_t>> tuple_list;
+	list<list<element_t>> tuple_list, wait_list;
+        string send2client;
 
         while (true) {
                 int client;
@@ -34,6 +37,7 @@ int main () {
                 list<element_t> tuple;
 
                 fgets(buf, sizeof(buf), stdin);
+                strtok(buf, "\n");
                 if (!strncmp(buf, "exit", 4))    break;
 
                 p = strtok(buf, " ");
@@ -43,6 +47,8 @@ int main () {
                 p = strtok(NULL, " ");
                 do {
                         element_t ele;
+                        ele.client_id = client;
+                        printf("input with id %d\n", ele.client_id);
 
                         if (p[0]=='"') {                /* string */
                                 printf("string\n");
@@ -50,9 +56,13 @@ int main () {
                                 strcpy(ele.content_str, p);
                         }
                         else if (p[0]=='?') {    /* undefined variable */
-                                printf("variable\n");
+                                printf("undefined variable\n");
                                 ele.type = VAR;
+                                strcpy(ele.var_name, &p[1]);
                                 // insert to variable list
+                        }
+                        else if (!isdigit(p[0])) {      /* defined variable */
+                                printf("defined variable %s\n", p);
                         }
                         else {    /* int */
                                 printf("integer\n");
@@ -63,15 +73,132 @@ int main () {
 
                         p = strtok(NULL, " ");
                 } while (p);    // til end of input buffer parsing
-                tuple_list.push_back(tuple);
-        }
+                
+                // responsing logic
+                send2client.clear();
+                if (!strcmp(act, "out")) {      // out
+                        bool match = false;
+                        int ret2client_id;
+                        for (auto out_it = wait_list.begin(); (match == false)&&(out_it != wait_list.end()); out_it++) {
+                        //for (auto out_it = tuple_list.begin(); (match == false)&&(out_it != tuple_list.end()); out_it++) {
+                                auto tuple_it = tuple.begin();
+                                for (auto it = out_it->begin(); it != out_it->end(); it++) {
+                                        match = true;
+                                        ret2client_id = it->client_id;
+                                        
+                                        // check length
+                                        if (tuple.size()!=out_it->size()) {
+                                                match = false;
+                                                break;
+                                        }
 
-        // print tuple list
-        for (auto out_it = tuple_list.begin(); out_it != tuple_list.end(); out_it++) {
-                for (auto it = out_it->begin(); it != out_it->end(); it++) {
-                        cout << "type " << it->type << ", content " << it->content_int << endl;
+                                        // compare elements one by one
+                                        if ((it->type == tuple_it->type)&&(it->type==INT)) {
+                                                if (it->content_int == tuple_it->content_int) {
+                                                        send2client += to_string(tuple_it->content_int);
+                                                        send2client += " ";
+                                                        tuple_it++;
+                                                }
+                                                else {
+                                                        match = false;
+                                                        break;
+                                                }
+                                        }
+                                        else if ((it->type == tuple_it->type)&&(it->type==STR)) {
+                                                if (!strcmp(it->content_str, tuple_it->content_str)) {
+                                                        send2client += string(tuple_it->content_str);
+                                                        send2client += " ";
+                                                        tuple_it++;
+                                                }
+                                                else {
+                                                        match = false;
+                                                        break;
+                                                }
+                                        }
+                                        else {
+                                                // TODO variable
+                                        }
+                                }
+                        }
+                        cout << "send2client:" << send2client <<  "match " << match << "client " << ret2client_id << endl;
+                        if (match == false)      // no previous request match
+                                tuple_list.push_back(tuple);
                 }
-        }
+                else {  // read or in
+                        bool match = false;
+                        //for (auto out_it = wait_list.begin(); (match == false)&&(out_it != wait_list.end()); out_it++) {
+                        for (auto out_it = tuple_list.begin(); (match == false)&&(out_it != tuple_list.end()); out_it++) {
+                                auto tuple_it = tuple.begin();
+                                for (auto it = out_it->begin(); it != out_it->end(); it++) {
+                                        match = true;
 
+                                        // check length
+                                        if (tuple.size()!=out_it->size()) {
+                                                match = false;
+                                                break;
+                                        }
 
+                                        // compare elements one by one
+                                        if ((it->type == tuple_it->type)&&(it->type==INT)) {
+                                                if (it->content_int == tuple_it->content_int) {
+                                                        send2client += to_string(tuple_it->content_int);
+                                                        send2client += " ";
+                                                        tuple_it++;
+                                                }
+                                                else {
+                                                        match = false;
+                                                        break;
+                                                }
+                                        }
+                                        else if ((it->type == tuple_it->type)&&(it->type==STR)) {
+                                                if (!strcmp(it->content_str, tuple_it->content_str)) {
+                                                        send2client += string(tuple_it->content_str);
+                                                        send2client += " ";
+                                                        tuple_it++;
+                                                }
+                                                else {
+                                                        match = false;
+                                                        break;
+                                                }
+                                        }
+                                        else {
+                                                // TODO variable
+                                        }
+                                }
+                        }
+                        cout << "send2client:" << send2client <<  "match " << match << endl;
+                        if (match == false)      // no previous request match
+                                wait_list.push_back(tuple);
+                }
+
+                // print tuple list
+                cout << "----------------------------\n";
+                for (auto out_it = tuple_list.begin(); out_it != tuple_list.end(); out_it++) {
+                        for (auto it = out_it->begin(); it != out_it->end(); it++) {
+                                if (it->type == INT)
+                                        cout << " " << it->content_int;
+                                else if (it->type == VAR)
+                                        cout << " " << it->var_name;
+                                else 
+                                        cout << " " << it->content_str;
+                        }
+                        cout << "\n";
+                }
+                cout << "----------------------------\n";
+                // print waiting list
+                cout << "****************************\n";
+                for (auto out_it = wait_list.begin(); out_it != wait_list.end(); out_it++) {
+                        cout << "ask by client " << (out_it->begin())->client_id << endl;
+                        for (auto it = out_it->begin(); it != out_it->end(); it++) {
+                                if (it->type == INT)
+                                        cout << " " << it->content_int;
+                                else if (it->type == VAR)
+                                        cout << " " << it->var_name;
+                                else 
+                                        cout << " " << it->content_str;
+                        }
+                        cout << "\n";
+                }
+                cout << "****************************\n";
+        }   
 }
